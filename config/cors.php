d237a05fcdc26120a5af8360ee1a2f3ec6c83681
hadjbakir <hadjbakirbvb.16@gmail.com>
2025-08-10 23:39:50 +0100

    cursor modification

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://easy-com-front-pied.vercel.app',
+        'https://easy-com-front-pied.vercel.app/'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
